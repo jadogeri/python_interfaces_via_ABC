0f86156714d31369a3401e2c2b7eaa2e2e280777
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-04-17 16:24:21 -0500

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from src.bitcoin import BitCoin
+from src.credit_card import CreditCard
+from src.debit_card import DebitCard
+from src.interfaces.payment import Payment
+
+
 def main()-> None:
-    bitcoin: BitCoin = BitCoin();
-    bitcoin.process(30.5);
+    payments: list[tuple(Payment, float)]  = [
+        (BitCoin(), 4000),
+        (CreditCard(), 1000),
+        (DebitCard(), 2000)
+    ]
+
+    ''' Loop through all payments '''
+    for payment,amount in payments:
+        payment.process(amount);
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
